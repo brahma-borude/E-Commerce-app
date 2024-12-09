5acf34a99ed3119512c827e40bec9ed8ced7c038
Brahma <brahmaborude2005@gmail.com>
2024-12-09 09:43:04 +0000

    ok

--- FlaskMarket/market.py
+++ FlaskMarket/market.py
@@ -1,11 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "<h1>Hello, World!</h1>"
-
-@app.route('/about')
-def about_page():
-    return "<p>This is a about page</p>"
+def home_page():
+    return render_template("home.html")
+    
